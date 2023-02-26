c35b8a5d04766c71778038da440ba12db950d18f
harshal3007 <harshalbhawsar9@gmail.com>
2023-02-26 12:21:59 +0530

    Day-5 progress

---
+++ furniture-frenzy/src/Components/Pages/Cart.jsx
@@ -0,0 +1,12 @@
+import { Footer } from "../Footer/footer";
+import { Navbar } from "../Header/NavBar";
+
+export function Cart(){
+    return (
+        <>
+        <Navbar/>
+        <div>Carts</div>
+        <Footer/>
+        </>
+    )
+}
